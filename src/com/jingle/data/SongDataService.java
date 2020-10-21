a5295c254c0771b325c04b6ea5501c821995105a
Henry-Harvey <46501451+Henry-Harvey@users.noreply.github.com>
2020-10-20 20:51:30 -0700

    Added create and readAll for SongDataService

--- src/com/jingle/data/SongDataService.java
+++ src/com/jingle/data/SongDataService.java
@@ -11,8 +11,8 @@ import java.util.List;
 import com.jingle.model.Song;
 
 /**
- * @author Henry Harvey
- * The SongDataService modifies and retrieves data from the songs database
+ * @author Henry Harvey The SongDataService modifies and retrieves data from the
+ *         songs database
  */
 
 public class SongDataService implements DataAccessInterface<Song> {
@@ -21,9 +21,41 @@ public class SongDataService implements DataAccessInterface<Song> {
 	 * @see DataAccessInterface create
 	 */
 	public int create(Song song) {
-		return 0;
+		int result = 0;
+		Database db = new Database();
+		Connection conn = null;
+
+		String sql = "INSERT INTO songs (title, artist, album, year, length, genre, users_id) VALUES (?,?,?,?,?,?,?)";
+
+		try {
+			conn = db.getConnection();
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, song.getTitle());
+			stmt.setString(2, song.getArtist());
+			stmt.setString(3, song.getAlbum());
+			stmt.setString(4, song.getYear());
+			stmt.setString(5, song.getLength());
+			stmt.setString(6, song.getGenre());
+			stmt.setInt(7, song.getUsers_id());
+
+			if (stmt.executeUpdate() != 1) {
+				result = -1;
+				throw new SQLException("Creating song failed.");
+			}
+		} catch (java.sql.SQLException e) {
+			throw new DatabaseException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (java.sql.SQLException e) {
+					throw new DatabaseException(e);
+				}
+			}
+		}
+		return result;
 	}
-	
+
 	/**
 	 * not implemented
 	 */
@@ -31,15 +63,41 @@ public class SongDataService implements DataAccessInterface<Song> {
 		return null;
 	}
 
+	/**
+	 * not implemented
+	 */
 	public List<Song> readByTitle(Song song) {
-		List<Song> result = new ArrayList<Song>();
-		result.add(new Song(song.getTitle(), "", "2020", "00:10", ""));
-		return result;
+		return null;
 	}
-	
+
 	public List<Song> readAll() {
 		List<Song> result = new ArrayList<Song>();
-		result.add(new Song());
+		Database db = new Database();
+		Connection conn = null;
+
+		String sql = "SELECT * FROM songs";
+
+		try {
+			conn = db.getConnection();
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				result.add(new Song(rs.getInt("id"), rs.getString("title"), rs.getString("artist"),
+						rs.getString("album"), rs.getString("year"), rs.getString("length"), rs.getString("genre"),
+						rs.getInt("users_id")));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					throw new DatabaseException(e);
+				}
+			}
+		}
 		return result;
 	}
 
--- src/com/jingle/model/Song.java
+++ src/com/jingle/model/Song.java
@@ -3,8 +3,8 @@ package com.jingle.model;
 import javax.validation.constraints.Size;
 
 /**
- * @author Henry Harvey
- * The Song model is the product object model of the application
+ * @author Henry Harvey The Song model is the product object model of the
+ *         application
  */
 
 public class Song {
@@ -13,7 +13,7 @@ public class Song {
 	@Size(min = 1, max = 60, message = "Title must be between 1 and 60 characters long.")
 	private String title;
 
-	private int artist_id;
+	private String artist;
 
 	@Size(min = 1, max = 60, message = "Album must be between 1 and 60 characters long.")
 	private String album;
@@ -27,37 +27,43 @@ public class Song {
 	@Size(min = 1, max = 30, message = "Genre must be between 1 and 30 characters long.")
 	private String genre;
 
+	private int users_id;
+
 	// Constructor with all fields
-	public Song(int id, String title, int artist_id, String album, String year, String length, String genre) {
+	public Song(int id, String title, String artist, String album, String year, String length, String genre,
+			int users_id) {
 		this.id = id;
 		this.title = title;
-		this.artist_id = artist_id;
+		this.artist = artist;
 		this.album = album;
 		this.year = year;
 		this.length = length;
 		this.genre = genre;
+		this.users_id = users_id;
 	}
 
-	// Constructor with all fields except id's
+	// Constructor with all fields except id's & artist
 	public Song(String title, String album, String year, String length, String genre) {
 		this.id = -1;
 		this.title = title;
-		this.artist_id = -1;
+		this.artist = "";
 		this.album = album;
 		this.year = year;
 		this.length = length;
 		this.genre = genre;
+		this.users_id = -1;
 	}
 
 	// Default constructor
 	public Song() {
 		this.id = -1;
 		this.title = "title";
-		this.artist_id = -1;
+		this.artist = "";
 		this.album = "album";
 		this.year = "0000";
 		this.length = "00:00";
 		this.genre = "genre";
+		this.users_id = -1;
 	}
 
 	public int getId() {
@@ -76,12 +82,12 @@ public class Song {
 		this.title = title;
 	}
 
-	public int getArtist_id() {
-		return artist_id;
+	public String getArtist() {
+		return artist;
 	}
 
-	public void setArtist_id(int artist_id) {
-		this.artist_id = artist_id;
+	public void setArtist(String artist) {
+		this.artist = artist;
 	}
 
 	public String getAlbum() {
@@ -116,10 +122,17 @@ public class Song {
 		this.genre = genre;
 	}
 
-	@Override
-	public String toString() {
-		return "Song [id=" + id + ", title=" + title + ", artist_id=" + artist_id + ", album=" + album + ", year="
-				+ year + ", length=" + length + ", genre=" + genre + "]";
+	public int getUsers_id() {
+		return users_id;
 	}
 
+	public void setUsers_id(int users_id) {
+		this.users_id = users_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", title=" + title + ", artist=" + artist + ", album=" + album + ", year=" + year
+				+ ", length=" + length + ", genre=" + genre + ", users_id=" + users_id + "]";
+	}
 }

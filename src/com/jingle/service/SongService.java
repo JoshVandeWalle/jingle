0f8020c8ad1fd093fb8ed3a87e4ba1a25eeda1b2
JoshVandeWalle <joshv98@icloud.com>
2020-11-27 14:54:56 -0800

    Added basic REST API

---
+++ src/com/jingle/service/SongService.java
@@ -0,0 +1,54 @@
+package com.jingle.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jingle.business.SongBusinessInterface;
+import com.jingle.model.Song;
+import com.jingle.util.RestDto;
+
+/**
+ * The Song service manages API behavior for songs
+ * @author Josh Van de Walle
+ *
+ */
+@RestController()
+@RequestMapping("/api")
+public class SongService {
+	// business service used to enforce business rules and business logic
+	SongBusinessInterface songBusinessService;
+
+	// setter injection happens here
+	@Autowired
+	public void setSongBusinessService(SongBusinessInterface songBusinessService) {
+		this.songBusinessService = songBusinessService;
+	}
+	// get all songs method
+	@GetMapping("/retrieveAll")
+	public RestDto<Song> handleRetrieveAll()
+	{
+		// use try catch to handle exceptions
+		try 
+		{
+			// pass control to business layer to retrieve all songs
+			List<Song> songs = songBusinessService.getAllSongs();
+			
+			if (songs.size() > 0)
+				// If all goes right, send the songs
+				return new RestDto<Song>(200, "OK", songs);
+			else
+				// no songs found
+				return new RestDto<Song>(404, "Songs not found", songs);
+		}
+		
+		// handle exceptions here
+		catch (Exception e)
+		{
+			return new RestDto<Song>(500, "Internal error", null);
+		}
+	}
+}
---
+++ src/com/jingle/util/RestDto.java
@@ -0,0 +1,30 @@
+package com.jingle.util;
+
+import java.util.List;
+
+public class RestDto<T> {
+	private int code;
+	private String message;
+	private List<T> data;
+	public RestDto(int code, String message, List<T> data) {
+		super();
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+	public RestDto() {
+		super();
+	}
+	public int getCode() {
+		return code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public List<T> getData() {
+		return data;
+	}
+	
+	
+
+}
